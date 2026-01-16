27211695d661645f526792148fbc8bb39799439e
botong101 <faithcheclaire.viscayno.22@usjr.edu.ph>
2026-01-17 02:06:53 +0800

    Fix: Add fileReplacements for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://manggasense.up.railway.app/api'  //backend url
+  
   //apiUrl: 'http://192.168.1.18:8000/api' 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://mangoapi.up.railway.app/api'  
-  apiUrl: 'http://manggasense.up.railway.app/api' 
+  apiUrl: 'https://manggasense.up.railway.app/api' 
 };
