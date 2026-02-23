b6219091be1839c6c340835261981147d0a75321
botong101 <faithcheclaire.viscayno.22@usjr.edu.ph>
2026-02-23 16:01:28 +0800

    fixed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://manggasense.up.railway.app/api'  //backend url
+  apiUrl: 'https://manggasense.up.railway.app/api'  //backend url
   
-  apiUrl: 'http://127.0.0.1:8000/api' 
+  //apiUrl: 'http://127.0.0.1:8000/api' 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://mangoapi.up.railway.app/api'  
-  //apiUrl: 'https://manggasense.up.railway.app/api' 
-  apiUrl: 'http://127.0.0.1:8000/api'
+  apiUrl: 'https://manggasense.up.railway.app/api' 
+  //apiUrl: 'http://127.0.0.1:8000/api'
 };
